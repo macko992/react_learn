a6548474381d0da00efb5870770029f33b6df297
macko992 <m4ck0992@gmail.com>
2018-05-16 15:23:10 +0200

    Start learn React - default project

---
+++ src/components/Text.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Text extends Component {
+  render() {
+    return (<p>
+      <div>
+        <p>ssdfadsf</p>
+      <h1> Nagłowek </h1>
+      </div>
+    </p>);
+  }
+}
+
+export default Text;
